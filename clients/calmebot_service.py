b91a6b3da7df2dc87e2bb7f7c734535253b3ccdc
llucasmoura2 <154241503+llucasmoura2@users.noreply.github.com>
2024-06-10 15:32:47 -0300

    Update calmebot_service.py

--- clients/calmebot_service.py
+++ clients/calmebot_service.py
@@ -4,10 +4,14 @@ class CallMeBot:
     
     def __init__(self):
         self.__base_url = 'https://api.callmebot.com/whatsapp.php'
-        self.__api_key = '9771278'
+        self.__api_key = 'sua_api_key'
 
     def send_message(self, message):
         response = requests.get(
-            url = f'{self.__base_url}?phone=+557192209258&text={message}&apikey={self.__api_key}'
+            url = f'{self.__base_url}?phone=+55758888888&text={message}&apikey={self.__api_key}'
         )
         return response.text
+
+# site usado para api do call me bot "https://www.callmebot.com/blog/free-api-whatsapp-messages/"
+# coloque sua api key com base nos dados do site
+# troque o telefone na linha 11 para seu numero
